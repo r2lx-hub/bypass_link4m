a51c6466f42584cc71a6724a28b10506291f1e0e
r2lx-hub <r2lxhub@gmail.com>
2025-07-18 22:28:56 +0700

    Add files via upload

---
+++ tampermonkey.user.js
@@ -0,0 +1,11 @@
+// ==UserScript==
+// @name         Bypass Link4m.com (Tool)
+// @namespace    http://tampermonkey.net/
+// @version      Beta
+// @description  Auto vượt xLink_mixvip không cần chạm tay
+// @author       By R2lx
+// @require      https://raw.githubusercontent.com/r2lx-hub/bypass_link4m/refs/heads/main/bypass-link4m.js
+// @match        https://link4m.com/*
+// @match        https://*.link4m.com/*
+// @grant        none
+// ==/UserScript==
